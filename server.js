c8ca3c4cd2b058e51b52e9aebec1af8d99fba62f
Muhammed Furkan Aydın <furkanaydin@turkdeveloper.com>
2017-11-23 14:15:30 +0300

    Yeni bilgiler de eklendi

--- server.js
+++ server.js
@@ -19,31 +19,113 @@ if (!TwitterConfig.access_token_secret) throw new Error("Twitter access token se
 var client = new Twitter(TwitterConfig);
 
 const timer = setInterval(_ => {
-    https.get("https://www.doviz.com/api/v1/currencies/USD/latest", (res) => {
-        if(res.statusCode != 200) return console.log("Bağlantı problemi !")
-        let dump = ""
-        res.setEncoding('utf8');
-        res.on('data', (d) => {
-            dump += d
-        });
 
-        res.on("end", _ => {
-            var data = JSON.parse(dump);
-            const date = new Date();
-            client.post('statuses/update', {
-                    status: `#dolar alış fiyatı : ${data.buying} ve Satış Fiyatı : ${data.selling} \n \n Tarih : ${date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()}`
-                })
-                .then(function (tweet) {
-                    removeInterval(timer)
-                    console.log("Tweet paylaşıldı. Alış fiyatı, " + data.buying + " ve satış fiyatı, " + data.selling)
-                })
-                .catch(function (error) {
-                    console.log(new Date())
-                    console.log(error)
-                })
-        })
-
-    }).on('error', (e) => {
-        console.error(e);
-    });
 }, 900000)
+
+
+function getInformations() {
+    // Altının son değerlerini alıyorum
+    const altin = new Promise((resolve, reject) => {
+        https.get("https://www.doviz.com/api/v1/golds/ons/latest", (res) => {
+            if (res.statusCode != 200) return reject("Bağlantı problemi !")
+            let dump = ""
+            res.setEncoding('utf8');
+            res.on('data', (d) => {
+                dump += d
+            });
+            res.on("end", _ => {
+                var data = JSON.parse(dump);
+                resolve(data)
+            })
+
+        }).on('error', (e) => {
+            reject(e)
+        });
+    })
+
+
+    // Tüm döviz bilgilerini çekiyorum...
+    const doviz = new Promise((resolve, reject) => {
+        https.get("https://www.doviz.com/api/v1/currencies/all/latest", (res) => {
+            if (res.statusCode != 200) return reject("Bağlantı problemi !")
+            let dump = ""
+            res.setEncoding('utf8');
+            res.on('data', (d) => {
+                dump += d
+            });
+
+            res.on("end", _ => {
+                const data = JSON.parse(dump);
+
+                //Sadece belirlemiş olduğum kurlar üzerinde işlem yapmak istiyorum. Bu yüzden bir liste oluşturalım. 
+                // USD, EUR ve GBP yeterli olucaktır
+                const currenyList = {
+                    USD: {},
+                    EUR: {},
+                    GBP: {}
+                };
+                data.forEach(el => {
+                    if (currenyList[el.code]) {
+                        currenyList[el.code].buying = el.buying;
+                        currenyList[el.code].selling = el.selling;
+                    }
+                })
+                return resolve(currenyList)
+            })
+
+        }).on('error', (e) => {
+            reject(e)
+        });
+    })
+
+    const bist100 = new Promise((resolve, reject) => {
+        https.get("https://www.doviz.com/api/v1/indexes/XU100/latest", (res) => {
+            if (res.statusCode != 200) return reject("Bağlantı problemi !")
+            let dump = ""
+            res.setEncoding('utf8');
+            res.on('data', (d) => {
+                dump += d
+            });
+
+            res.on("end", _ => {
+                var data = JSON.parse(dump);
+                resolve(data)
+            })
+
+        }).on('error', (e) => {
+            reject(e)
+        });
+    })
+
+    Promise.all([altin, doviz, bist100]).then(results => {
+        const date = new Date()
+        const tarihBilgisi = `Güncelleme: ${date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()}`
+
+        const altinBilgisi = `\n\n#ALTIN ALIŞ - ${results[0].buying}\n#ALTIN SATIŞ - ${results[0].selling}`;
+
+        const dovizBilgisi = `\n\n#USD ALIŞ - ${results[1].USD.buying}\n#USD SATIŞ - ${results[1].USD.selling}\n\n#EUR ALIŞ - ${results[1].EUR.buying}\n#EUR SATIŞ - ${results[1].EUR.selling}\n\n#GBP ALIŞ - ${results[1].GBP.buying}\n#GBP SATIŞ - ${results[1].GBP.selling}`;
+
+        const bistBilgisi = `\n\n#BIST100 - ${results[2].latest}`;
+
+
+        client.post('statuses/update', {
+                status: `${tarihBilgisi + altinBilgisi + dovizBilgisi + bistBilgisi}`
+            })
+            .then(function (tweet) {
+                console.log("Tweet paylaşıldı. Alış fiyatı, " + data.buying + " ve satış fiyatı, " + data.selling)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+/**
+                
+ * 
+ * 
+ */
+
+getInformations()
